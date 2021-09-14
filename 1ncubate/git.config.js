5f0474d9545d23e2c398365f1f72fef357cf2d09
crosshj <github@chimpjuice.com>
2021-09-14 19:19:53 -0400

    notes about plugins/extensions/store + git.config.js

--- 1ncubate/git.config.js
+++ 1ncubate/git.config.js
@@ -1,51 +1,46 @@
 import ini from 'https://cdn.skypack.dev/ini';
 
-const ConfigProxy = (config) => {
-	let proxy;
-	const set =  (target, key, value, receiver) => {
-		//console.log(`setting ${wrapper.url}/${key} !`);
-		return Reflect.set(target, key, value, receiver);
-	};
-	const get = (target, key, receiver) => {
-		const propertyExists = target[key] ||
-			['toJSON', 'toObject', 'then'].includes(key);
-		if(propertyExists) return Reflect.get(target, key, receiver);
-		target[key] = {};
-		return ConfigProxy(target[key]);
-	};
-	proxy = new Proxy(config, { get, set });
-	return proxy;
-};
-
 class GitConfig {
-	constructor({ local }={}){
-		this.url = local
-			? 'local'
-			: 'https://beta.fiug.dev/.git/config';
+	constructor({ root }={}){
+		this.url = (root || location.origin) + '/.git/config';
+	}
+	async update(fn){
+		await this.read();
+		fn(this.config);
+		await this.save();
 	}
 	async read(){
 		const configText = await fetch(this.url).then(x=> x.ok ? x.text() : undefined);
-		return ConfigProxy(
+		const ConfigProxy = (config) => {
+			const get = (target, key, receiver) => {
+				const ignore = target[key] ||
+					['toJSON', 'toObject', 'then'].includes(key);
+				if(ignore) return Reflect.get(target, key, receiver);
+				target[key] = {};
+				return ConfigProxy(target[key]);
+			};
+			return new Proxy(config, { get });
+		};
+		this.config = ConfigProxy(
 			configText
 				? ini.parse(configText)
-				: {},
-			this
+				: {}
 		);
 	}
-	async save(config){
-		console.log(`save config to ${this.url}:\n`);
+	async save(){
+		const { config } = this;
+		console.log(`\nsave config to ${this.url}:\n`);
 		console.log(JSON.stringify(config, null, 2)+'\n\n');
 		console.log(ini.encode(config, { whitespace: true }) + '\n\n');
 	}
 }
 
-const localConfig = new GitConfig();
-const config = await localConfig.read();
-config.user.email = 'foo@email.com';
-config.user.name = 'user';
 
-config.other.foo.hey = true;
-config.other.wow = true;
+new GitConfig({ root: cwd }).update(config => {
+	config.user.email = 'foo@email.com';
+	config.user.name = 'user';
 
+	config.other.foo.hey = true;
+	config.other.wow = true;
+});
 
-await localConfig.save(config);
--- 1ncubate/store/store.js
+++ 1ncubate/store/store.js
@@ -1,4 +1,15 @@
-// TODO: fiug-plugins wrapped in a store abstraction
+/*
 
-// the people from eclipse theia did this, may be worth looking at
-// https://open-vsx.org/
+TODO: fiug-plugins wrapped in a store abstraction
+
+the people from eclipse theia did this, may be worth looking at
+https://open-vsx.org/
+https://open-vsx.org/swagger-ui/
+
+
+building a VS Code extension seems to require Visual Studio (lame)
+https://docs.microsoft.com/en-us/visualstudio/extensibility/the-structure-of-the-content-types-dot-xml-file?view=vs-2019
+https://github.com/igorskyflyer/vscode-pack-vsix
+https://fek.io/blog/how-to-create-a-vs-code-extension-with-java-script
+
+*/
