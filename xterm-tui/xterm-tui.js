0521d65bbc341fd1e3a93a152a4b43e7cf5db890
crosshj <github@chimpjuice.com>
2021-09-20 01:11:42 -0400

    xterm-tui and comlink experiment init

---
+++ xterm-tui/xterm-tui.js
@@ -0,0 +1,14 @@
+/*
+
+https://github.com/RangerMauve/xterm-tui
+
+in order to make this work, need terminal to be exposed to scripts running in terminal
+right now it's only exposed using console.log
+
+use https://github.com/GoogleChromeLabs/comlink
+
+
+
+*/
+
+import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
